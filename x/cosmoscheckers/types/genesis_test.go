778280ac3e5d93bb7cfbc7437f00fa487f3d9b9d
Aw Kai Shin <kaishinaw@Aws-MacBook-Pro.local>
2022-07-16 18:30:17 +0800

    First unit test

--- x/cosmoscheckers/types/genesis_test.go
+++ x/cosmoscheckers/types/genesis_test.go
@@ -7,59 +7,68 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestGenesisState_Validate(t *testing.T) {
-	for _, tc := range []struct {
-		desc     string
-		genState *types.GenesisState
-		valid    bool
-	}{
-		{
-			desc:     "default is valid",
-			genState: types.DefaultGenesis(),
-			valid:    true,
-		},
-		{
-			desc: "valid genesis state",
-			genState: &types.GenesisState{
+// func TestGenesisState_Validate(t *testing.T) {
+// 	for _, tc := range []struct {
+// 		desc     string
+// 		genState *types.GenesisState
+// 		valid    bool
+// 	}{
+// 		{
+// 			desc:     "default is valid",
+// 			genState: types.DefaultGenesis(),
+// 			valid:    true,
+// 		},
+// 		{
+// 			desc: "valid genesis state",
+// 			genState: &types.GenesisState{
 
-				NextGame: &types.NextGame{
-					IdValue: 28,
-				},
-				StoredGameList: []types.StoredGame{
-					{
-						Index: "0",
-					},
-					{
-						Index: "1",
-					},
-				},
-				// this line is used by starport scaffolding # types/genesis/validField
-			},
-			valid: true,
+// 				NextGame: &types.NextGame{
+// 					IdValue: 28,
+// 				},
+// 				StoredGameList: []types.StoredGame{
+// 					{
+// 						Index: "0",
+// 					},
+// 					{
+// 						Index: "1",
+// 					},
+// 				},
+// 				// this line is used by starport scaffolding # types/genesis/validField
+// 			},
+// 			valid: true,
+// 		},
+// 		{
+// 			desc: "duplicated storedGame",
+// 			genState: &types.GenesisState{
+// 				StoredGameList: []types.StoredGame{
+// 					{
+// 						Index: "0",
+// 					},
+// 					{
+// 						Index: "0",
+// 					},
+// 				},
+// 			},
+// 			valid: false,
+// 		},
+// 		// this line is used by starport scaffolding # types/genesis/testcase
+// 	} {
+// 		t.Run(tc.desc, func(t *testing.T) {
+// 			err := tc.genState.Validate()
+// 			if tc.valid {
+// 				require.NoError(t, err)
+// 			} else {
+// 				require.Error(t, err)
+// 			}
+// 		})
+// 	}
+// }
+
+func TestDefaultGenesisIsCorrect(t *testing.T) {
+	require.EqualValues(t,
+		&GenesisState{
+			StoredGameList: []*StoredGame{},
+			NextGame:       &NextGame{"", uint64(1)},
 		},
-		{
-			desc: "duplicated storedGame",
-			genState: &types.GenesisState{
-				StoredGameList: []types.StoredGame{
-					{
-						Index: "0",
-					},
-					{
-						Index: "0",
-					},
-				},
-			},
-			valid: false,
-		},
-		// this line is used by starport scaffolding # types/genesis/testcase
-	} {
-		t.Run(tc.desc, func(t *testing.T) {
-			err := tc.genState.Validate()
-			if tc.valid {
-				require.NoError(t, err)
-			} else {
-				require.Error(t, err)
-			}
-		})
-	}
+		DefaultGenesis())
 }
