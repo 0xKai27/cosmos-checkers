d2d6dd87419ac19abd51358992b943e77845fd8a
Aw Kai Shin <kaishinaw@Aws-MacBook-Pro.local>
2022-07-16 18:12:26 +0800

    Add storedGame map

--- vue/src/store/generated/OxKai27/cosmos-checkers/oxkai27.cosmoscheckers.cosmoscheckers/module/rest.ts
+++ vue/src/store/generated/OxKai27/cosmos-checkers/oxkai27.cosmoscheckers.cosmoscheckers/module/rest.ts
@@ -19,8 +19,27 @@ export interface CosmoscheckersNextGame {
  */
 export type CosmoscheckersParams = object;
 
+export interface CosmoscheckersQueryAllStoredGameResponse {
+  storedGame?: CosmoscheckersStoredGame[];
+
+  /**
+   * PageResponse is to be embedded in gRPC response messages where the
+   * corresponding request message has used PageRequest.
+   *
+   *  message SomeResponse {
+   *          repeated Bar results = 1;
+   *          PageResponse page = 2;
+   *  }
+   */
+  pagination?: V1Beta1PageResponse;
+}
+
 export interface CosmoscheckersQueryGetNextGameResponse {
   NextGame?: CosmoscheckersNextGame;
+}
+
+export interface CosmoscheckersQueryGetStoredGameResponse {
+  storedGame?: CosmoscheckersStoredGame;
 }
 
 /**
@@ -29,6 +48,14 @@ export interface CosmoscheckersQueryGetNextGameResponse {
 export interface CosmoscheckersQueryParamsResponse {
   /** params holds all the parameters of this module. */
   params?: CosmoscheckersParams;
+}
+
+export interface CosmoscheckersStoredGame {
+  index?: string;
+  game?: string;
+  turn?: string;
+  red?: string;
+  black?: string;
 }
 
 export interface ProtobufAny {
@@ -40,6 +67,69 @@ export interface RpcStatus {
   code?: number;
   message?: string;
   details?: ProtobufAny[];
+}
+
+/**
+* message SomeRequest {
+         Foo some_parameter = 1;
+         PageRequest pagination = 2;
+ }
+*/
+export interface V1Beta1PageRequest {
+  /**
+   * key is a value returned in PageResponse.next_key to begin
+   * querying the next page most efficiently. Only one of offset or key
+   * should be set.
+   * @format byte
+   */
+  key?: string;
+
+  /**
+   * offset is a numeric offset that can be used when key is unavailable.
+   * It is less efficient than using key. Only one of offset or key should
+   * be set.
+   * @format uint64
+   */
+  offset?: string;
+
+  /**
+   * limit is the total number of results to be returned in the result page.
+   * If left empty it will default to a value to be set by each app.
+   * @format uint64
+   */
+  limit?: string;
+
+  /**
+   * count_total is set to true  to indicate that the result set should include
+   * a count of the total number of items available for pagination in UIs.
+   * count_total is only respected when offset is used. It is ignored when key
+   * is set.
+   */
+  count_total?: boolean;
+
+  /**
+   * reverse is set to true if results are to be returned in the descending order.
+   *
+   * Since: cosmos-sdk 0.43
+   */
+  reverse?: boolean;
+}
+
+/**
+* PageResponse is to be embedded in gRPC response messages where the
+corresponding request message has used PageRequest.
+
+ message SomeResponse {
+         repeated Bar results = 1;
+         PageResponse page = 2;
+ }
+*/
+export interface V1Beta1PageResponse {
+  /** @format byte */
+  next_key?: string;
+
+  /** @format uint64 */
+  total?: string;
 }
 
 export type QueryParamsType = Record<string | number, any>;
@@ -265,6 +355,48 @@ export class Api<SecurityDataType extends unknown> extends HttpClient<SecurityDa
   queryParams = (params: RequestParams = {}) =>
     this.request<CosmoscheckersQueryParamsResponse, RpcStatus>({
       path: `/OxKai27/cosmos-checkers/cosmoscheckers/params`,
+      method: "GET",
+      format: "json",
+      ...params,
+    });
+
+  /**
+   * No description
+   *
+   * @tags Query
+   * @name QueryStoredGameAll
+   * @summary Queries a list of StoredGame items.
+   * @request GET:/OxKai27/cosmos-checkers/cosmoscheckers/stored_game
+   */
+  queryStoredGameAll = (
+    query?: {
+      "pagination.key"?: string;
+      "pagination.offset"?: string;
+      "pagination.limit"?: string;
+      "pagination.count_total"?: boolean;
+      "pagination.reverse"?: boolean;
+    },
+    params: RequestParams = {},
+  ) =>
+    this.request<CosmoscheckersQueryAllStoredGameResponse, RpcStatus>({
+      path: `/OxKai27/cosmos-checkers/cosmoscheckers/stored_game`,
+      method: "GET",
+      query: query,
+      format: "json",
+      ...params,
+    });
+
+  /**
+   * No description
+   *
+   * @tags Query
+   * @name QueryStoredGame
+   * @summary Queries a StoredGame by index.
+   * @request GET:/OxKai27/cosmos-checkers/cosmoscheckers/stored_game/{index}
+   */
+  queryStoredGame = (index: string, params: RequestParams = {}) =>
+    this.request<CosmoscheckersQueryGetStoredGameResponse, RpcStatus>({
+      path: `/OxKai27/cosmos-checkers/cosmoscheckers/stored_game/${index}`,
       method: "GET",
       format: "json",
       ...params,
